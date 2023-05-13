3a0766301ac9d68034d7e847b66ab2508c5b476a
Eloy-SC <eloy.sancho.cebrero@gmail.com>
2023-05-14 00:35:33 +0200

    cambios

--- src/fp/crimen/CrimenesImpl.java
+++ src/fp/crimen/CrimenesImpl.java
@@ -2,14 +2,12 @@ package fp.crimen;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
